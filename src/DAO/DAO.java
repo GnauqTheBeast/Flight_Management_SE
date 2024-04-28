37ed77c7d7e47b0b8b912bde4d55c16df9fb9f87
QuangThinkpad <quangnguyenngoc314@gmail.com>
2024-04-28 23:52:53 +0700

    first commit

---
+++ src/DAO/DAO.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.util.*;
+import java.sql.*;
+
+/**
+ *
+ * @author Quang Nguyen
+ */
+public class DAO {
+    private static String connectionString = "jdbc:sqlserver://localhost:1434;databaseName=test;user=sa;password=Duckie@01;encrypt=false";
+
+    private Connection _connect;
+
+    public DAO() {
+
+        _connect = initializeDBConnection();
+    }
+
+    private Connection initializeDBConnection() {
+        try {
+            String password = "";
+            Connection connection = DriverManager.getConnection(connectionString);
+            System.out.println("OK");
+            return connection;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/cnpm/CNPM.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cnpm;
+
+import DAO.DAO;
+
+/**
+ *
+ * @author Quang Nguyen
+ */
+public class CNPM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DAO deptrai = new DAO();
+    }
+    
+}
